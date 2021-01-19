65f88f74cd76333b76b39844c9ca6947051406ca
dinhocamp <essidsleheddine@gmail.com>
2021-01-19 14:39:51 +0100

    project setup

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.port || 8080;
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
