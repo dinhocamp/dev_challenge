fa177139a6b6abc8ed1a30be093259352b485f97
dinhocamp <essidsleheddine@gmail.com>
2021-01-19 21:58:55 +0100

    database setup and started setting up the server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const db = require("./models/index");
+require("dotenv").config();
 const app = express();
-const port = process.env.port || 8080;
+const port = process.env.port;
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-
+require("./routes/images.routes")(app);
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("inside db");
+  })
+  .catch((err) => {
+    console.log("error from db connection ", err);
+  });
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
