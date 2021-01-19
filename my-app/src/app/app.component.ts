5a2eaee3e5295666f7f821912069fc0f1a678215
dinhocamp <essidsleheddine@gmail.com>
2021-01-19 19:09:05 +0100

    rendering the search input tag along the input tag  type file

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-app';
+  searchedItem = null;
+  changed(e: any) {
+    this.searchedItem = e.target.value;
+    console.log(this.searchedItem);
+  }
 }
